cc6304ea1ef5b77dda82c4e2d25028300ba80247
omaru <omarux@gmail.com>
2017-10-09 20:09:14 -0600

    weather app

--- app/src/main/java/com/londonappbrewery/climapm/WeatherController.java
+++ app/src/main/java/com/londonappbrewery/climapm/WeatherController.java
@@ -1,34 +1,53 @@
 package com.londonappbrewery.climapm;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 
 public class WeatherController extends AppCompatActivity {
 
     // Constants:
-    final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
+    private static final int REQUESTED_CODE=123;
+    private final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
     // App ID to use OpenWeather data
-    final String APP_ID = "e72ca729af228beabd5d20e3b7749713";
+    private final String APP_ID = "e72ca729af228beabd5d20e3b7749713";
     // Time between location updates (5000 milliseconds or 5 seconds)
-    final long MIN_TIME = 5000;
+    private final long MIN_TIME = 5000;
     // Distance between location updates (1000m or 1km)
-    final float MIN_DISTANCE = 1000;
+    private final float MIN_DISTANCE = 1000;
 
-    // TODO: Set LOCATION_PROVIDER here:
-
-
+    private static final String LOCATION_PROVIDER = LocationManager.GPS_PROVIDER;
 
 
     // Member Variables:
-    TextView mCityLabel;
-    ImageView mWeatherImage;
-    TextView mTemperatureLabel;
-
-    // TODO: Declare a LocationManager and a LocationListener here:
+    private TextView mCityLabel;
+    private ImageView mWeatherImage;
+    private TextView mTemperatureLabel;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
 
     @Override
@@ -41,36 +60,133 @@ public class WeatherController extends AppCompatActivity {
         mWeatherImage = (ImageView) findViewById(R.id.weatherSymbolIV);
         mTemperatureLabel = (TextView) findViewById(R.id.tempTV);
         ImageButton changeCityButton = (ImageButton) findViewById(R.id.changeCityButton);
-
-
-
-        // TODO: Add an OnClickListener to the changeCityButton here:
-
+        changeCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WeatherController.this,ChangeCityController.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
-    // TODO: Add onResume() here:
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Clima", "resuming ... getting weather for location");
+        Intent intent = getIntent();
+        String city = intent.getStringExtra("city");
+        if (city != null){
+            getWeatherForNewCity(city);
+        }else{
+            getWeatherForCurrentLocation();
+
+        }
 
 
+    }
 
-    // TODO: Add getWeatherForNewCity(String city) here:
-
-
-
-    // TODO: Add getWeatherForCurrentLocation() here:
-
-
-
-    // TODO: Add letsDoSomeNetworking(RequestParams params) here:
-
+    private void getWeatherForNewCity(String city) {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        RequestParams params = new RequestParams();
+        params.put("q",city);
+        params.put("appid",APP_ID);
+        letsDoSomeNetworking(params);
+    }
 
 
+    private void getWeatherForCurrentLocation() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("Clima", "location changed");
+                String longitude = String.valueOf(location.getLongitude());
+                String latitude = String.valueOf(location.getLatitude());
+                Log.d("Clima", "latitude"+latitude+" longitude"+longitude);
+                RequestParams params = new RequestParams();
+                params.put("lat",latitude);
+                params.put("lon",longitude);
+                params.put("appid",APP_ID);
+                params.put("units","metric");
+                letsDoSomeNetworking(params);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Log.d("Clima", "location provider disabled");
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUESTED_CODE);
+            return;
+        }
+        locationManager.requestLocationUpdates(LOCATION_PROVIDER, MIN_TIME, MIN_DISTANCE, locationListener);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUESTED_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getWeatherForCurrentLocation();
+
+            }else{
+                Log.d("Clima", "Permission for Location Denied");
+            }
+        }
+    }
+    private void letsDoSomeNetworking(RequestParams params){
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(WEATHER_URL,params,new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        Log.d("Clima", "SUCCESS JSON" + response.toString());
+                        updateUI(WeatherDataModel.fromJson(response));
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                        Log.e("Clima", "Fail" + errorResponse.toString());
+                        Log.d("Clima", "statusCode" + statusCode);
+                        Toast.makeText(WeatherController.this, "Request failed", Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
     // TODO: Add updateUI() here:
 
-
+    private void updateUI(WeatherDataModel model){
+        mTemperatureLabel.setText(model.getTemperature());
+        mCityLabel.setText(model.getCity());
+        int resourceId = getResources().getIdentifier(model.getIconName(),"drawable",getPackageName());
+        mWeatherImage.setImageResource(resourceId);
+    }
 
     // TODO: Add onPause() here:
-
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(locationManager != null){
+            locationManager.removeUpdates(locationListener);
+        }
+    }
 
 
 }
--- app/src/main/java/com/londonappbrewery/climapm/WeatherDataModel.java
+++ app/src/main/java/com/londonappbrewery/climapm/WeatherDataModel.java
@@ -1,47 +1,97 @@
 package com.londonappbrewery.climapm;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.util.Log;
+
 public class WeatherDataModel {
 
     // TODO: Declare the member variables here
+    private String temperature;
+    private int condition;
+    private String city;
+    private String iconName;
+
+    public WeatherDataModel(){
+
+    }
+
+    public static final WeatherDataModel fromJson(JSONObject jsonObject){
+        WeatherDataModel model = new WeatherDataModel();
+        try{
+            model.setTemperature(jsonObject.getJSONObject("main").getString("temp"));
+            model.setCity(jsonObject.getString("name"));
+            model.setCondition(jsonObject.getJSONArray("weather").getJSONObject(0).getInt("id"));
+            model.setIconName(model.updateWeatherIcon(model.getCondition()));
+        }catch(JSONException e){
+            Log.w("Clima","Unable to parse clima info "+e.toString());
+        }
+        return model;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getIconName() {
+        return iconName;
+    }
+
+    public void setIconName(String iconName) {
+        this.iconName = iconName;
+    }
 
 
-    // TODO: Create a WeatherDataModel from a JSON:
+    private static String updateWeatherIcon(int condition) {
 
+        if (condition >= 0 && condition < 300) {
+            return "tstorm1";
+        } else if (condition >= 300 && condition < 500) {
+            return "light_rain";
+        } else if (condition >= 500 && condition < 600) {
+            return "shower3";
+        } else if (condition >= 600 && condition <= 700) {
+            return "snow4";
+        } else if (condition >= 701 && condition <= 771) {
+            return "fog";
+        } else if (condition >= 772 && condition < 800) {
+            return "tstorm3";
+        } else if (condition == 800) {
+            return "sunny";
+        } else if (condition >= 801 && condition <= 804) {
+            return "cloudy2";
+        } else if (condition >= 900 && condition <= 902) {
+            return "tstorm3";
+        } else if (condition == 903) {
+            return "snow5";
+        } else if (condition == 904) {
+            return "sunny";
+        } else if (condition >= 905 && condition <= 1000) {
+            return "tstorm3";
+        }
 
-    // TODO: Uncomment to this to get the weather image name from the condition:
-//    private static String updateWeatherIcon(int condition) {
-//
-//        if (condition >= 0 && condition < 300) {
-//            return "tstorm1";
-//        } else if (condition >= 300 && condition < 500) {
-//            return "light_rain";
-//        } else if (condition >= 500 && condition < 600) {
-//            return "shower3";
-//        } else if (condition >= 600 && condition <= 700) {
-//            return "snow4";
-//        } else if (condition >= 701 && condition <= 771) {
-//            return "fog";
-//        } else if (condition >= 772 && condition < 800) {
-//            return "tstorm3";
-//        } else if (condition == 800) {
-//            return "sunny";
-//        } else if (condition >= 801 && condition <= 804) {
-//            return "cloudy2";
-//        } else if (condition >= 900 && condition <= 902) {
-//            return "tstorm3";
-//        } else if (condition == 903) {
-//            return "snow5";
-//        } else if (condition == 904) {
-//            return "sunny";
-//        } else if (condition >= 905 && condition <= 1000) {
-//            return "tstorm3";
-//        }
-//
-//        return "dunno";
-//    }
-
-    // TODO: Create getter methods for temperature, city, and icon name:
-
+        return "dunno";
+    }
 
 
 }
